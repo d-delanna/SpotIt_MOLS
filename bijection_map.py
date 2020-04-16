1f55ac163e4620019e549c3e4fe39498909d0aa8
d-delanna <52421314+d-delanna@users.noreply.github.com>
2020-04-15 22:52:23 -0500

    Update bijection_map.py

--- bijection_map.py
+++ bijection_map.py
@@ -1,11 +1,7 @@
 import itertools
 import numpy as np
 
-# TODO: add comments for SpotIt_to_MOLS
-
-class MOLS_to_SpotIt:
-    
-    # TODO: should make copies of MOLS instead of directly mutating
+class MOLS_to_SpotIt:   
     """ Converts a set of MOLS of any siz and converts them to SpotIt! cards. 
         param: mols: type< list(list(ints)) >: latin squares """
     
@@ -13,8 +9,9 @@ class MOLS_to_SpotIt:
         self.mols = mols
         self.size = len(mols[0])
         
-    def convert_to_spotit(self, square, group_num):
+    def convert_to_spotit(self, table, group_num):
         """ converts single latin square to group """
+        square = table.copy()
         for line in square:
             for i in range(self.size):
                 line[i] = line[i] + (self.size+2) + self.size*i
@@ -25,20 +22,20 @@ class MOLS_to_SpotIt:
         """ compiles full game into list containing all cards """
         start_card = [[i+1 for i in range(self.size+1)]]
         group2 = [[2] + [(self.size+2) + (self.size*i) + (j) for i in range(self.size)] for j in range(self.size)]
-        group1 = [[1]+list(line) for line in np.array(self.group2).T[1:]]
+        group1 = [[1]+list(line) for line in np.array(group2).T[1:]]
 
-        self.game = [start_card, group1, self.group2]
+        self.game = [start_card, group1, group2]
         for i, mol in enumerate(self.mols):
             self.game += [self.convert_to_spotit(mol, i+3)]
         return self.game
     
     def __str__(self):
-        """ prints full SpotIt! game """
+        """ string of full SpotIt! game """
         print("SpotIt Game")
+        print_game = []
         for group in self.full_game():
-            [print(line) for line in group]
-            print('\n')
-        return str(self.full_game())
+            print_game += [str(line) + '\n' for line in group] + ['\n']
+        return ''.join(print_game)
         
     def print_group(self, group_num):
         """ prints single group """
@@ -46,12 +43,18 @@ class MOLS_to_SpotIt:
         [print(line) for line in self.full_game()[group_num]]
         print('\n')
 
- class SpotIt_to_MOLS:
+        
+class SpotIt_to_MOLS:
+    """ conversion from SpotIt! game to its generating MOLS 
+        param: game: list[starting card, group1, group2, ..., groupN]"""
+    
     def __init__(self, game):
-        self.game = game[2:]
+        self.game = game[3:]
         self.size = len(self.game[0])
         
     def convert_to_ls(self, group):
+        """ takes group from SpotIt and returns its latin square generator """
+        #[print(line) for line in group]
         latin_square = [[(num - (self.size+2) - self.size*i) for i, num in enumerate(line[1:])] for line in group]
         return latin_square
     
@@ -61,35 +64,28 @@ class MOLS_to_SpotIt:
     
     def __str__(self):
         print("MOLS(" + str(self.size) + ")")
+        print_mols = []
         for mol in self.compile_mols():
-            [print(line) for line in mol]
-            print('\n')
-        return str(self.compile_mols())
+            print_mols += [str(line) + '\n' for line in mol] + ['\n']
+        return ''.join(print_mols)
+    
     
 
 class Check:
     """ tests to verify if nxn tables are MOLS 
-        param: tables: type< list(list(ints)): supposed latin squares """
+        param: tables: type< list(list(ints)) >: supposed latin squares """
     
     def __init__(self, *tables):
         self.tables = tables
         self.size = len(tables[0])
         
-    def __compile(self, table1, table2):
-        return [[table for table in itertools.zip_longest(table1[i], table2[i])] for i in range(self.size)]
-    
     def __flatten(self, table):
         return [tab for tab in itertools.chain.from_iterable(table)]
         
-    def latin_square(self, table):
-        """ checks if a square is a latin square"""
-        for row in table:
-            if len(row) != len(set(row)):
-                return False
-        for column in np.array(table).T:
-            if len(column) != len(set(column)):
-                return False
-        return True
+    def compiler(self, table1, table2):
+        """ superimposes table2 onto table1: list(list(length-2 tuples))"""
+        compiled_table = [[table for table in itertools.zip_longest(table1[i], table2[i])] for i in range(self.size)]
+        return self.__flatten(compiled_table)
     
     def __repeated_pairs(self, table):
         """ if not orthogonal, will print out all repeated pairs """
@@ -100,10 +96,22 @@ class Check:
                 print("Repeated {} at (row, column): {}".format(table[0], coord_repeat_pairs))
                 [table.pop(idx) for idx in repeat_pairs[::-1]]
             return self.__repeated_pairs(table)
+        
+    def latin_square(self, table):
+        """ checks if a square is a latin square"""
+        for row in table:
+            if len(row) != len(set(row)):
+                # self.__repeated_pairs(table)
+                return False
+        for column in np.array(table).T:
+            if len(column) != len(set(column)):
+                # self.__repeated_pairs(table)
+                return False
+        return True
     
     def orthogonal(self, table1, table2):
         """ checks if two latin squares form Graeco-Latin square """
-        table = self.__flatten(self.__compile(table1, table2))
+        table = self.compiler(table1, table2)
         if len(table) == len(set(table)):
             return True
         self.__repeated_pairs(table)
@@ -117,7 +125,8 @@ class Check:
         orthog = [self.orthogonal(tab[0], tab[1]) for tab in itertools.combinations(self.tables,2)]
         if False in orthog:
             return False
-        return True   
+        return True        
+
         
  # EXAMPLE MOLS ---------------------------------------------
  # LS3 and LS4 orthogonal
@@ -166,7 +175,7 @@ LS5 = [
 game = MOLS_to_SpotIt(LS3, LS4)
 print(game)
 
-mols = SpotIt_to_MOLS(LS3, LS4)
+mols = SpotIt_to_MOLS(game.full_game())
 print(mols)  # should return back to original form
 
 check = Check(LS3, LS4, LS5)
